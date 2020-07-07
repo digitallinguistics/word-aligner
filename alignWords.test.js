4603b8dd5aa46d2cc44f7c1d1b25acbe372f42c5
Danny Hieber <dwhieb@gmail.com>
2020-07-06 21:03:38 -0500

    write failing test

--- alignWords.test.js
+++ alignWords.test.js
@@ -33,6 +33,28 @@ describe(`alignWords`, function() {
 
   });
 
+  it.only(`aligns by character length`, function() {
+
+    const lines = [
+      `t̓aatn̓a ʔunaakʔaɬ`,
+      `children they.have`,
+    ];
+
+    const expectedResult = [
+      `t̓aatn̓a   ʔunaakʔaɬ`,
+      `children they.have`,
+    ];
+
+    const aligned = alignWords(lines);
+
+    aligned.forEach((line, i) => {
+
+      expect(line).to.be(expectedResult[i]);
+
+    });
+
+  });
+
   it(`groups words with [brackets]`, function() {
 
     const lines = [
